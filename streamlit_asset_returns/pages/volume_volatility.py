b7e842960cf53c38c6cf1f20e2f161e23a5d7214
simongarisch <gatman946@gmail.com>
2020-06-16 07:38:30 +1000

    adding to volume volatility section

--- streamlit_asset_returns/pages/volume_volatility.py
+++ streamlit_asset_returns/pages/volume_volatility.py
@@ -1,4 +1,5 @@
 import numpy as np
+import altair as alt
 import streamlit as st
 from bokeh.plotting import figure
 from . data import (
@@ -9,8 +10,36 @@ from . data import (
 from .. import util
 
 
+def altair_volume_volatility_scatter(df):
+    return alt.Chart(df).mark_circle(size=60).encode(
+        x="Volume",
+        y="AbsReturn",
+        tooltip=[
+            alt.Tooltip("Volume", format=",.0f"),
+            alt.Tooltip("AbsReturn", format=".4f"),
+        ],
+    ).properties(
+        title="Absolute Return and Volume",
+        width=600,
+        height=400,
+    ).interactive()
+
+
 def volume_volatility():
     st.title("Volume Volatility Correlation")
     st.markdown("""
         Trading volume and volatility are positively correlated.
     """)
+
+    st.sidebar.subheader("Ticker")
+    ticker = create_ticker_picker()
+
+    df = get_pricing_data(ticker)
+    df["AbsReturn"] = df["Returns"].abs()
+
+    if st.checkbox("Show source code"):
+        st.markdown(util.python_code_markdown(
+            altair_volume_volatility_scatter
+        ))
+
+    st.altair_chart(altair_volume_volatility_scatter(df))
