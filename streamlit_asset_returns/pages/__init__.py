31b9ff9c286035dabcdba6839bceaafd8ea9c148
simongarisch <gatman946@gmail.com>
2020-06-15 17:24:22 +1000

    adding autocorr to imports

--- streamlit_asset_returns/pages/__init__.py
+++ streamlit_asset_returns/pages/__init__.py
@@ -4,3 +4,4 @@ from .approx_zero import approx_zero  # noqa: F401
 from .peaked import peaked  # noqa: F401
 from .gain_loss import gain_loss  # noqa: F401
 from .vol_clustering import vol_clustering  # noqa: F401
+from .autocorr import autocorr  # noqa: F401
